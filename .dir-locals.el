d3bff9c00298c0035573c0ac7d225676a7cdef33
Florian Stinglmayr <fstinglmayr@gmail.com>
2018-10-09 21:21:22 +0200

    more content and fixes

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,4 @@
-((nil . ;; global configuration settings for this project
-      (ispell-dictionary "en_GB")
-      (mode . flyspell)
-      (set-fill-column 78)
-      )
- )
+((nil . ((ispell-dictionary . "en_GB")
+         (mode . flyspell)
+         (fill-column . 78)))
+ 
