e6683f644f28b552a764a5c79401746a39126309
Florian Stinglmayr <fstinglmayr@gmail.com>
2018-10-25 23:28:17 +0100

    also set UTF-8 and unix

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,7 @@
 ((nil . ((ispell-dictionary . "en_GB")
          (mode . flyspell)
          (mode . whitespace)
-         (fill-column . 78))))
+         (fill-column . 78)
+         (buffer-file-coding-system . utf-8-unix)
+         )))
  
